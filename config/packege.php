e4bed3133c9a612badbac6c5cb2d1a67e3384e8d
ShafiulNaeem <shafiul15-1052@diu.edu.bd>
2023-10-05 14:33:35 +0600

    packege initiaige

---
+++ config/packege.php
@@ -0,0 +1,69 @@
+<?php
+
+return [
+    /**
+     * If true then the package will create a new controller
+     * according to the module name
+     */
+
+    'controller' => true,
+
+    /**
+     * If true then the package will create a new model
+     * according to the module name
+     */
+    'model' => true,
+
+    /**
+     * If true then the package will create a new seeder
+     * according to the module name
+     */
+
+    'seeder' => true,
+
+    /**
+     * If true then the package will create a new request
+     * according to the module name
+     */
+
+    'request' => true,
+
+    /**
+     * If true then the package will create a new view
+     * according to the module name
+     */
+
+    'view' => true,
+
+    /**
+     * If true then the package will create a new migration
+     * according to the module name
+     */
+
+    'migration' => true,
+
+    /**
+     * If true then the package will create a new route
+     * according to the module name
+     */
+
+    'route' => true,
+
+    /**
+     * If true then the package will create a Controller with Resource
+     * according to the module name
+     */
+    'resource' => true,
+
+    /**
+     * If true then the package will create a Request with FormRequest
+     * according to the module name
+     */
+    'formRequest' => true,
+
+    /**
+     * The package will create route
+     * according to the prefix name
+     */
+    'routePrefix' => 'permission_',
+];
